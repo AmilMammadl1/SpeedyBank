e222d3c912d98a50dd104c994ed7bf0c90285149
AmilMammadl1 <amilanimasiya02@gmail.com>
2024-02-27 18:06:40 +0400

    structure is ready

--- src/main/java/com/ms001/bank/dto/request/ATMRequestDTO.java
+++ src/main/java/com/ms001/bank/dto/request/ATMRequestDTO.java
@@ -1,4 +1,10 @@
 package com.ms001.bank.dto.request;
 
+import com.ms001.bank.constant.SupportedServicesType;
+
 public class ATMRequestDTO {
+    private String location;
+    private String status;
+    private SupportedServicesType supportedServices;
+    private Long bankId;
 }
